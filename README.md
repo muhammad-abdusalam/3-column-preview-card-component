b8a42d9afc05952b3781bba1d81546cc61dbeac8
Muhammad-adam778 <Muhammad.adam4078@gmail.com>
2022-12-07 20:59:11 +0200

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# 3-column-preview-card-component
+# Frontend Mentor - 3-column preview card component solution
+
+This is a solution to the [3-column preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Screenshot
+
+![Desktop](./screenshot/img-1.png)
+![Mobile](./screenshot/img-2.png)
+
+### Demo
+
+https://muhammad-adam778.github.io/3-column-preview-card-component/
---
+++ main.js
@@ -0,0 +1,24 @@
+// ##Catch Elements
+const carBoxs = document.querySelectorAll(".cars-preview .car");
+
+let arrayOfCars = Array.from(carBoxs);
+
+// ##Events
+window.onload = function () {
+  if (scrollY >= 370) {
+    // Update animation delay for last card
+    arrayOfCars[arrayOfCars.length - 1].style.animationDelay = "0.4s";
+  }
+  arrayOfCars.forEach((car) => moveOnScroll(car));
+};
+window.onscroll = function () {
+  arrayOfCars.forEach((car) => moveOnScroll(car));
+};
+
+// ##Functions
+// Show Columns On Scroll
+function moveOnScroll(car) {
+  if (scrollY >= car.offsetTop - 550) {
+    car.classList.add("animate");
+  }
+}
