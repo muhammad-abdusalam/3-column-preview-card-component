f045c70f6aff87ca5390d3981df7d2ddc00238c5
Muhammad-adam778 <Muhammad.adam4078@gmail.com>
2022-12-08 10:30:41 +0200

    Improve Accessibility, Design, Animation

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ let arrayOfCars = Array.from(carBoxs);
 
 // ##Events
 window.onload = function () {
-  if (scrollY >= 370) {
+  if (scrollY >= 275) {
     // Update animation delay for last card
-    arrayOfCars[arrayOfCars.length - 1].style.animationDelay = "0.4s";
+    arrayOfCars[2].style.animationDelay = "0.3s";
   }
   arrayOfCars.forEach((car) => moveOnScroll(car));
 };
